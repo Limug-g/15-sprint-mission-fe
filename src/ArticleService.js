968f2c7d2953e4f284cb3c22778590133e4dd191
Limug-g <dmlrbs94@gmail.com>
2026-08-01 15:30:04 +0900

    feature: Fix all function by using then() instead of async,await

--- src/ArticleService.js
+++ src/ArticleService.js
@@ -2,119 +2,124 @@
 const BASE_URL = "https://panda-market-api-crud.vercel.app";
 
 //GET 메소드 이용 : getArticleList 만들기-> 게시글 전체 조회
-async function getArticleList({
+function getArticleList({
   page = 1,
   pageSize = 10,
   orderBy = "recent",
   keyword = "",
 } = {}) {
-  try {
-    const response = await fetch(
-      `${BASE_URL}/articles?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`,
-      { method: "GET" },
-    );
+  return fetch(
+    `${BASE_URL}/articles?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`,
+    { method: "GET" },
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP 에러: ${response.statusText}`);
+      }
 
-    if (!response.ok) {
-      throw new Error(`HTTP 에러: ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
-    console.log(result);
-  } catch (error) {
-    console.log("게시글을 조회하지 못했습니다.: ", error.message);
-  }
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("게시글을 조회하지 못했습니다.: ", error.message);
+    });
 }
 getArticleList();
 
-//GET 메소드 이용 : getArticle 만들기-> 특정 게시글 조회
-async function getArticle(articleId) {
-  try {
-    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
-      method: "GET",
+// //GET 메소드 이용 : getArticle 만들기-> 특정 게시글 조회
+function getArticle(articleId) {
+  return fetch(`${BASE_URL}/articles/${articleId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP 에러: ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("게시글을 찾을 수 없습니다.: ", error.message);
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP 에러: ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
-    console.log(result);
-  } catch (error) {
-    console.log("게시글을 찾을 수 없습니다.: ", error.message);
-  }
 }
 getArticle(6736);
 
-// //POST 메소드 이용 : createArticle 만들기-> 게시글 생성하기
-async function createArticle() {
-  try {
-    const response = await fetch(`${BASE_URL}/articles`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        image: "https://example.com/...",
-        content: "게시글 내용입니다.: 상품 상태 양호합니다.",
-        title: "상품 상태 양호",
-      }),
+// // // //POST 메소드 이용 : createArticle 만들기-> 게시글 생성하기
+function createArticle() {
+  return fetch(`${BASE_URL}/articles`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      image: "https://example.com/...",
+      content: "무야호",
+      title: "상품 상태 야호",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP 에러: ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("게시글을 생성할 수 없습니다.: ", error.message);
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP 에러: ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
-    console.log(result);
-  } catch (error) {
-    console.log("게시글을 생성할 수 없습니다.: ", error.message);
-  }
 }
 createArticle();
 
-//PATCH 메소드로 patchArticle()만들기-> 게시글 수정
-async function patchArticle(articleId) {
-  try {
-    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        image: "https://example2.com/...",
-        content: "어렵다잉",
-        title: "마 할 수 있나.",
-      }),
+// //PATCH 메소드로 patchArticle()만들기-> 게시글 수정
+function patchArticle(articleId) {
+  return fetch(`${BASE_URL}/articles/${articleId}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      image: "https://example2.com/...",
+      content: "어렵다잉",
+      title: "마 할 수 있나.",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP 에러: ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log("게시글을 수정할 수 없습니다.: ", error.message);
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP 에러: ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
-    console.log(result);
-  } catch (error) {
-    console.log("게시글을 수정할 수 없습니다.: ", error.message);
-  }
 }
-patchArticle(6740);
+patchArticle(6755);
 
-// DELETE 메소드로 게시글 지우기
-async function deleteArticle(articleId) {
-  try {
-    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
-      method: "DELETE",
+// // DELETE 메소드로 게시글 지우기
+function deleteArticle(articleId) {
+  return fetch(`${BASE_URL}/articles/${articleId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP 에러: ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then((result) => {
+      console.log("삭제된 게시글 id: ", result);
+    })
+    .catch((error) => {
+      console.log("삭제 실패:", error.message);
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP 에러: ${response.statusText}`);
-    }
-
-    const result = await response.json();
-    console.log("삭제된 게시글 id: ", result);
-  } catch (error) {
-    console.log("삭제 실패:", error.message);
-  }
 }
-deleteArticle(6759);
-// getArticleList();
+deleteArticle(6760);
---
+++ src/ProductService.js
@@ -0,0 +1,120 @@
+//ProductService API 링크를 가져와서 CRUD 메소드를 함수로 작성해보기
+const BASE_URL = "https://panda-market-api-crud.vercel.app/products";
+
+//GET 메소드 이용 : getArticleList 만들기-> 게시글 전체 조회
+async function getProductList({
+  page = 1,
+  pageSize = 10,
+  orderBy = "recent",
+  keyword = "",
+} = {}) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`,
+      { method: "GET" },
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP 에러: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
+    console.log(result);
+  } catch (error) {
+    console.log("상품을 조회하지 못했습니다.: ", error.message);
+  }
+}
+getArticleList();
+
+//GET 메소드 이용 : getArticle 만들기-> 특정 게시글 조회
+async function getArticle(articleId) {
+  try {
+    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP 에러: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
+    console.log(result);
+  } catch (error) {
+    console.log("게시글을 찾을 수 없습니다.: ", error.message);
+  }
+}
+getArticle(6736);
+
+// //POST 메소드 이용 : createArticle 만들기-> 게시글 생성하기
+async function createArticle() {
+  try {
+    const response = await fetch(`${BASE_URL}/articles`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        image: "https://example.com/...",
+        content: "게시글 내용입니다.: 상품 상태 양호합니다.",
+        title: "상품 상태 양호",
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP 에러: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
+    console.log(result);
+  } catch (error) {
+    console.log("게시글을 생성할 수 없습니다.: ", error.message);
+  }
+}
+createArticle();
+
+//PATCH 메소드로 patchArticle()만들기-> 게시글 수정
+async function patchArticle(articleId) {
+  try {
+    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        image: "https://example2.com/...",
+        content: "어렵다잉",
+        title: "마 할 수 있나.",
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP 에러: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log(`응답 성공! ${response.status}: ${response.statusText}`);
+    console.log(result);
+  } catch (error) {
+    console.log("게시글을 수정할 수 없습니다.: ", error.message);
+  }
+}
+patchArticle(6740);
+
+// DELETE 메소드로 게시글 지우기
+async function deleteArticle(articleId) {
+  try {
+    const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP 에러: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log("삭제된 게시글 id: ", result);
+  } catch (error) {
+    console.log("삭제 실패:", error.message);
+  }
+}
+deleteArticle(6759);
+// getArticleList();
