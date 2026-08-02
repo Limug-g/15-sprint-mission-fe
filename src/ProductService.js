32499291cd2362118b135032701eb3e91c8ed0ad
Limug-g <dmlrbs94@gmail.com>
2026-08-02 22:22:33 +0900

    feature: Add PATCH func in ProductService.js

--- src/ProductService.js
+++ src/ProductService.js
@@ -46,20 +46,50 @@ const BASE_URL = "https://panda-market-api-crud.vercel.app/products";
 // getProduct(4106);
 
 // //POST 메소드 이용 : createProduct 만들기-> 상품목록 생성하기
-async function createProduct() {
+// async function createProduct() {
+//   try {
+//     const response = await fetch(`${BASE_URL}`, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         images: ["https://example.com/..."],
+//         tags: ["완구"],
+//         //image와 tag 키 값은 여러 요소를 담을 수도 있으므로
+//         //배열 형태로 작성한다.
+//         //그리고 body 안에 작성하는 키 값들은 정해져 있는 것이다.
+//         price: 125000,
+//         description: "string",
+//         name: "Pikachu_toy",
+//       }),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP 에러: ${response.statusText}`);
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.log("상품 목록을 생성할 수 없습니다.: ", error.message);
+//   }
+// }
+// createProduct();
+
+//PATCH 메소드로 patchProduct()만들기-> 게시글 수정
+async function patchProduct(productId) {
   try {
-    const response = await fetch(`${BASE_URL}`, {
-      method: "POST",
+    const response = await fetch(`${BASE_URL}/${productId}`, {
+      method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         images: ["https://example.com/..."],
-        tags: ["완구"],
+        tags: ["가전기기"],
         //image와 tag 키 값은 여러 요소를 담을 수도 있으므로
         //배열 형태로 작성한다.
         //그리고 body 안에 작성하는 키 값들은 정해져 있는 것이다.
-        price: 125000,
+        price: 1_200_000,
         description: "string",
-        name: "Pikachu_toy",
+        name: "LG-Stand Air conditioner",
       }),
     });
 
@@ -70,36 +100,10 @@ async function createProduct() {
     const result = await response.json();
     console.log(result);
   } catch (error) {
-    console.log("상품 목록을 생성할 수 없습니다.: ", error.message);
+    console.log("상품 목록을 수정할 수 없습니다.: ", error.message);
   }
 }
-createProduct();
-
-//PATCH 메소드로 patchArticle()만들기-> 게시글 수정
-// async function patchArticle(articleId) {
-//   try {
-//     const response = await fetch(`${BASE_URL}/articles/${articleId}`, {
-//       method: "PATCH",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         image: "https://example2.com/...",
-//         content: "어렵다잉",
-//         title: "마 할 수 있나.",
-//       }),
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP 에러: ${response.statusText}`);
-//     }
-
-//     const result = await response.json();
-//     console.log(`응답 성공! ${response.status}: ${response.statusText}`);
-//     console.log(result);
-//   } catch (error) {
-//     console.log("게시글을 수정할 수 없습니다.: ", error.message);
-//   }
-// }
-// patchArticle(6740);
+patchProduct(4131);
 
 // DELETE 메소드로 게시글 지우기
 // async function deleteArticle(articleId) {
