a33ec2cde6384abccf19e8b22170b1e566d35898
Limug-g <dmlrbs94@gmail.com>
2026-08-26 23:26:57 +0900

    feature: fix gitignore file for avoid tracking the panda-market-Server folder

--- panda-market-Server/src/error/UnAuthorized-excep.js
+++
@@ -1,7 +0,0 @@
-import { HttpException } from './http-exception.js';
-
-export class UnAuthorizedException extends HttpException {
-  constructor(description = 'UNAUTHORIZED') {
-    super(401, description);
-  }
-}
